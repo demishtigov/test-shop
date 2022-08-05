92224e17b8124da2ba1f37e9d4b88a450ed14708
Magomed <93042393+demishtigov@users.noreply.github.com>
2022-08-05 21:52:22 +0300

    la

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import Items from "./components/items/Items";
+import Filter from "./components/filters/Filter";
 
 function App() {
-  return 
-  
+  return (
+    <>
+      <div className="container">
+        <aside className="aside__section">
+          <Filter />
+        </aside>
+        <main className="main__section">
+          <Items />
+        </main>
+      </div>
+    </>
+  );
 }
 
 export default App;
